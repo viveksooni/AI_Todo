1f8a277bb16bf4b4fc267ef6ff30f59104e88919
vivek soni <sonivivek346@gmail.com>
2025-06-28 15:33:36 +0530

    changed axios config again

--- client/src/lib/axiosConfig.js
+++ client/src/lib/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.DEV
+    ? "/api" // In development, use Vite proxy
+    : "/api", //
   timeout: 10000, // optional
   headers: {
     "Content-Type": "application/json",
